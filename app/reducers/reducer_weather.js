d8536ca3d854fb94236418cbdb5fa9b0b1308443
zlatan <zdimitrov@sevone.com>
2016-05-16 20:52:44 +0300

    Version Reverse

--- app/reducers/reducer_weather.js
+++ app/reducers/reducer_weather.js
@@ -1,12 +1,9 @@
 import { FETCH_WEATHER } from '../actions/index';
-import { SEARCH_BY } from '../actions/search';
 
 export default function(state = [], action) {
   switch (action.type) {
   case FETCH_WEATHER:
     return [ action.payload.data, ...state ];
-  case SEARCH_BY:
-      return [ action.payload.data, ...state ];
   }
   return state;
 }
